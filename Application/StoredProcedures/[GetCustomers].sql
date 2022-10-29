e42aa705053c68224c797a1aaa8b302d35960340
VinhHuynh <huynhquangvinh01121999@gmail.com>
2022-10-29 12:46:03 +0700

    handle output totalitem in store

--- Application/StoredProcedures/[GetCustomers].sql
+++ Application/StoredProcedures/[GetCustomers].sql
@@ -1,7 +1,7 @@
 ﻿USE [Software.Solution]
 GO
 
-/****** Object:  StoredProcedure [dbo].[GetCustomers]    Script Date: 29/10/2022 11:59:20 AM ******/
+/****** Object:  StoredProcedure [dbo].[GetCustomers]    Script Date: 29/10/2022 12:45:34 PM ******/
 SET ANSI_NULLS ON
 GO
 
@@ -10,10 +10,22 @@ GO
 
 CREATE PROCEDURE [dbo].[GetCustomers]
 	@pageNumber INT,
-	@pageSize INT
+	@pageSize INT,
+	@TotalItems INT OUTPUT
 AS
 BEGIN
-	SELECT * FROM Customers 
+	SELECT * into #tmpCustomers FROM Customers
+	
+	-- đếm tổng record query
+	DECLARE @sql_get_total NVARCHAR(max)
+	SET @sql_get_total = 'SELECT @TotalItems = COUNT(*) FROM #tmpCustomers';
+
+	EXEC sp_executesql 
+        @query = @sql_get_total, 
+        @params = N'@TotalItems INT OUTPUT', 
+        @TotalItems = @TotalItems OUTPUT;
+
+	select * from #tmpCustomers
 	ORDER BY Id OFFSET ((@pageNumber - 1) * @pageSize) ROWS FETCH NEXT @pageSize ROWS ONLY
 END
 GO
