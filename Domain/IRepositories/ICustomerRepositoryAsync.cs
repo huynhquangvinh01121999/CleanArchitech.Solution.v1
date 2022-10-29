655fe98521892b54e2f60e16aa9accac69257b2d
VinhHuynh <huynhquangvinh01121999@gmail.com>
2022-10-29 12:37:37 +0700

    add handle store procedure

--- Domain/IRepositories/ICustomerRepositoryAsync.cs
+++ Domain/IRepositories/ICustomerRepositoryAsync.cs
@@ -1,8 +1,11 @@
 ﻿using Domain.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Domain.IRepositories
 {
     public interface ICustomerRepositoryAsync : IBaseRepositoryAsync<Customers>
     {
+        Task<IList<Customers>> GetCustomers(int pageNumber, int pageSize);
     }
 }
--- Persistence/Repositories/CustomerRepositoryAsync.cs
+++ Persistence/Repositories/CustomerRepositoryAsync.cs
@@ -1,13 +1,39 @@
-﻿using Domain.Entities;
+﻿using Application.StoredProcedures;
+using Domain.Entities;
 using Domain.IRepositories;
 using Infrastructure;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Data;
+using System.Threading.Tasks;
 
 namespace Persistence.Repositories
 {
     public class CustomerRepositoryAsync : BaseRepositoryAsync<Customers>, ICustomerRepositoryAsync
     {
+        private readonly ApplicationDbContext _dbContext;
+
         public CustomerRepositoryAsync(ApplicationDbContext context) : base(context)
         {
+            _dbContext = context;
+        }
+
+        public async Task<IList<Customers>> GetCustomers(int pageNumber, int pageSize)
+        {
+            var parameter = new[]
+            {
+                new SqlParameter("@pageNumber",SqlDbType.Int) {Direction = ParameterDirection.Input, Value = pageNumber},
+                new SqlParameter("@pageSize",SqlDbType.Int) {Direction = ParameterDirection.Input, Value = pageSize}
+            };
+
+            string sql = string.Format("[{0}] @pageNumber, @pageSize", Procedures.GetCustomers);
+
+            var customers = await _dbContext.Set<Customers>()
+                                            .FromSqlRaw(sql.ToString(), parameter)
+                                            .ToListAsync();
+
+            return customers;
         }
     }
 }
